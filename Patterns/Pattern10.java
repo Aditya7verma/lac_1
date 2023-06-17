4e90cd1f0a4f505904bdc6e024d0e29ad681ccda
Aditya7verma <129338439+Aditya7verma@users.noreply.github.com>
2023-06-17 21:58:16 +0530

    Add files via upload

---
+++ Patterns/Pattern10.java
@@ -0,0 +1,33 @@
+package lac2_practice;
+
+import java.util.Scanner;
+
+public class Pattern10 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int row = 1;
+        int star = (n*2)-1;
+        int space = 0;
+
+        while (row <= n) {
+            //space
+            int i = 1;
+            while (i <= space) {
+                System.out.print("  ");
+                i++;
+            }
+            //star
+            int j = 1;
+            while (j <= star) {
+                System.out.print("*" + " ");
+                j++;
+            }
+            row++;
+            System.out.println();
+            space++;
+            star -=2;
+        }
+    }
+}
