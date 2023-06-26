32ca984b3799398dc30cfce64faa73d1cef5325d
Aditya7verma <129338439+Aditya7verma@users.noreply.github.com>
2023-06-26 20:33:57 +0530

    Add files via upload

---
+++ Patterns/Pattern25.java
@@ -0,0 +1,36 @@
+package Lac3;
+
+import java.util.Scanner;
+
+public class Pattern25 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int row =1;
+        int space =n- 1;
+        int star= 1;
+        int val =1;
+        while (row <= n){
+            //space
+            int i = 1;
+            while (i <= space){
+                System.out.print("\t");
+                i++;
+            }
+            //star
+            int j=1;
+            while (j<=star){
+                System.out.print(val + "\t");
+                val++;
+                j++;
+
+            }
+            space--;
+            star +=2;
+            System.out.println();
+            row++;
+        }
+
+    }
+}
