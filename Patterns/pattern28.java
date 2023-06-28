450755ae560dfe617601d6fb4c03ad5590a5d625
Aditya7verma <129338439+Aditya7verma@users.noreply.github.com>
2023-06-29 01:53:52 +0530

    Add files via upload

---
+++ Patterns/pattern28.java
@@ -0,0 +1,43 @@
+package Lac3;
+
+import java.util.Scanner;
+
+public class pattern28 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n= sc.nextInt();
+        int row = 1;
+        int star = 1;
+        int space = n-1;
+
+        while (row <= n){
+            //space
+            int i=1;
+            while (i <= space){
+                System.out.print("\t");
+                i++;
+            }
+            // star
+            int j= 1;
+            int val =row;
+
+            while (j <= star){
+
+                System.out.print(val + "\t");
+                if(j<=star/2){
+                    val++;
+                }
+                else {
+                    val--;
+                }
+                j++;
+
+            }
+            row++;
+            System.out.println();
+            space--;
+            star +=2;
+        }
+    }
+}
